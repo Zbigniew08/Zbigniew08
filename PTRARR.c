4791d0596c445e04a07912e3eb421a9f1096f935
Zbigniew Solski <94396036+Zbigniew08@users.noreply.github.com>
2022-06-12 17:33:47 +0200

    Update PTRARR.c

--- PTRARR.c
+++ PTRARR.c
@@ -2,19 +2,89 @@
 #include <stdlib.h>
 
 void dynamics( char*[], int);
+void print(char*[], int);
 
-int main( int arg, char *argv[])
+int main( int argc, char *argv[])
 {
     int iter = (int)(*(*(argv+1))) - '0';
-    char *sierpinski[iter];
-    dynamics( sierpinski, iter);
-
-    return 0;
+	if (argc<2)
+	{
+		printf("Podaj wartość tmax");
+	}
+	iter=atoi(argv[1]);
+	char *sierpinski[iter];
+	dynamics(sierpinski,iter);
+	print(sierpinski,iter);
+	return 0;
 }
 
-void dynamics( char *data[], int iter)
+
+void dynamics(char *data[], int iter)
 {
-    int i, k = 8;
-    data[0] = "A..#..B";
-    (data + 1) = malloc(k * sizeof(char));
+	int i, j, n, k = 9;
+	data[0] = "A00100B";
+ 	for (i=1; i<iter+1; i++, k+=2)
+    {
+  		*(data+i) = malloc(k * sizeof(char));
+		for (j=0, n=0; j<k-2; j++, n++)
+	    {
+			switch(*(*(data+i-1)+j))
+		  	{
+				case 'A':
+					*(*(data+i)+n)='A';
+					n+=1;
+					*(*(data+i)+n)='0';
+					break;
+			    case 'B':
+					*(*(data+i)+n)='0';
+					n+=1;
+					*(*(data+i)+n)='B';
+					break;
+				case '0':
+					if((*(*(data+i-1)+j-1)=='1') ^ (*(*(data+i-1)+j+1)=='1'))
+					{
+						*(*(data+i)+n)='1';
+					}
+					else
+					{
+						*(*(data+i)+n)='0';
+					}
+					break;
+		        case '1':
+					*(*(data+i)+n)='0';
+					break;
+		       
+			}
+		}
+    }
+}
+void print(char *data[], int iter)
+{
+    int i, j, k=7;
+    for (i=0; i<iter+1; i++, k+=2)
+    {
+	    for (j=iter-i; j>0; j--) 
+		{
+			printf(" ");
+		}
+	    for (j=0; j<k; j++)
+	    {
+		    switch (*(*(data+i)+j))
+		    {
+			    case 'A':
+			    	printf("A");
+			    	break;
+			    case 'B':
+			      	printf("B");
+			      	break;
+			    case '0':
+			      	printf("_");
+			      	break;
+			    case '1':
+			      	printf("#");
+			     	break;
+			}
+		}
+	}
+	printf("\n");
 }
